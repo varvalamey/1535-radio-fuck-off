9dab13315e9ed1ff6f72f029176250785945ae06
varvalamey <62106523+varvalamey@users.noreply.github.com>
2021-02-03 22:02:39 +0300

    Add files via upload

---
+++ play.js
@@ -0,0 +1,9 @@
+var checkbox = document.getElementById('11');
+var audio = document.getElementById('fuck');
+function playpause() {
+    if (checkbox.checked = true){
+        audi.play()
+    } else{
+        audio.paused()
+    }
+  }
